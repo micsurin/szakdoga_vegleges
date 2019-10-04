a46c391b4e354478520fa2e2dd55d90507feeb1e
jutomi <jutomika@gmail.com>
2019-10-04 17:40:26 +0200

    adc szazalek, pwm alapok

--- adc/main/adc1_example_main.c
+++ adc/main/adc1_example_main.c
@@ -1,11 +1,3 @@
-/* ADC1 Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
@@ -13,14 +5,18 @@
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
+#include "esp_attr.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
 
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
-static const adc_atten_t atten = ADC_ATTEN_DB_2_5;
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1;
+uint32_t percent = 0;
 
 
 
@@ -51,7 +47,6 @@ static void print_char_val_type(esp_adc_cal_value_t val_type)
         printf("Characterized using Default Vref\n");
     }
 }
-
 void app_main(void)
 {
     //Check if Two Point or Vref are burned into eFuse
@@ -84,11 +79,11 @@ void app_main(void)
             }
         }
         adc_reading /= NO_OF_SAMPLES;
-        kecske=adc_reading;
+        kecske=adc_reading/40.95;
         //Convert adc_reading to voltage in mV
         uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
         printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
-        fasz();
+        printf("%d  \n", percent);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
---
+++ adc/main/pwm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+#define LEDC_HS_TIMER          LEDC_TIMER_0
+#define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
+#define LEDC_HS_CH0_GPIO       (18)
+#define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
+
+void app_main(void)
+{
+    int ch;
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_13_BIT,
+        .freq_hz = 50000,
+        .speed_mode = LEDC_HS_MODE,
+        .timer_num = LEDC_HS_TIMER,
+        .clk_cfg = LEDC_AUTO_CLK,
+    }
+
+//LEDC EXAMPLE CHANNEL CONFIGTÓL FOLYTATNI!!
+
+
+}
